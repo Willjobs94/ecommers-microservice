9894fdfcba04496340d6cdf1d86e3c8208dbda55
William Bello <willjobs94@gmail.com>
2022-04-25 11:01:54 -0400

    Add Search API for Orders

---
+++ ECommerce.Api.Search/Services/OrderService.cs
@@ -0,0 +1,45 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using ECommerce.Api.Search.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+        private readonly ILogger<OrderService> logger;
+
+        public OrderService(IHttpClientFactory httpClientFactory, ILogger<OrderService> logger)
+        {
+            this.httpClientFactory = httpClientFactory;
+            this.logger = logger;
+        }
+
+        public async Task<(bool IsSuccess, IEnumerable<Order> Orders, string ErrorMessage)> GetOrderAsync(int customerId)
+        {
+            try
+            {
+                var client = httpClientFactory.CreateClient("OrdersService");
+                var response = await client.GetAsync($"api/orders/{customerId}");
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsByteArrayAsync();
+                    var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+                    var content = JsonSerializer.Deserialize<IEnumerable<Order>>(result, options);
+                    return (true, content, null);
+                }
+                return (false, null, response.ReasonPhrase);
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.ToString());
+                return (false, null, ex.Message);
+            }
+        }
+    }
+}
---
+++ ECommerce.Api.Search/Services/SearchService.cs
@@ -0,0 +1,31 @@
+﻿using ECommerce.Api.Search.Interfaces;
+using System.Threading.Tasks;
+
+namespace ECommerce.Api.Search.Services
+{
+    public class SearchService : ISearchService
+    {
+        private readonly IOrderService orderService;
+
+        public SearchService(IOrderService orderService)
+        {
+            this.orderService = orderService;
+        }
+
+        public async Task<(bool IsSuccess, dynamic SearchResult)> SearchAsync(int customerId)
+        {
+           var orderResult = await orderService.GetOrderAsync(customerId);
+
+            if (orderResult.IsSuccess)
+            {
+                var result = new
+                {
+                    Orders = orderResult.Orders
+                };
+
+                return (true, result);
+            }
+            return (false, null);
+        }
+    }
+}
